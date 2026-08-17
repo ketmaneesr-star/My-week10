a5f53e5599c22a8828cb91e12910f41c96fe38b0
ketmaneesr-star <ketmanee.sr@rmuti.ac.th>
2026-08-17 16:33:27 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,17 @@ Route::get('/abouts',[AdminController::class,"about"] )->name("abouts");
 Route::get('/blogs',[AdminController::class,"blog"] )->name("blogs");
 Route::get('/create',[AdminController::class,"create"] )->name("create");
 Route::post('/insert',[AdminController::class,"insert"] )->name("insert");
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "เชื่อมต่อฐานข้อมูลสำเร็จ! Database name: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        return "ไม่สามารถเชื่อมต่อฐานข้อมูลได้: " . $e->getMessage();
+    }
+}       );
+
 Route::get('blogs/delete/{id}', [AdminController::class, 'delete']);
 Route::get('/edit/{id}', [AdminController::class, 'edit'])->name('edit');
 Route::post('/update/{id}', [AdminController::class, 'update'])->name('update');
-Route::get('/change/{id}', [AdminController::class, 'change'])->name('change');
+Route::get('/blogs/change/{id}', [AdminController::class, 'change'])->name('change');
